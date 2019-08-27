9ea70a0744b6435029559ee5516c00aa0199e311
Hareesh-Ravi <hr268@scarletmail.rutgers.edu>
2019-08-27 19:25:32 -0400

    added parser to run.py

--- run.py
+++ run.py
@@ -10,64 +10,110 @@ import baseline
 import process_vist
 import vggfeat_vist
 import get_cohvec_vist
+import argparse
+
 
 if __name__ == '__main__':
+    
+    #argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--preprocess', action='store_true', default=False, 
+                        help='use this argument to preprocess VIST data')
+    parser.add_argument('--pretrain', action='store_true', default=False, 
+                        help='use this to pre-trainstage 1 of the network')
+    parser.add_argument('--train', type=str, default='stage1', 
+                        help ='train stage1, cnsi, nsi or baseline')
+    parser.add_argument('--eval', type=str, default='stage1', 
+                        help ='evaluate cnsi, nsi or baseline')
+    parser.add_argument('--show', type=str, default='stage1', 
+                        help ='show the story for cnsi, nsi or baseline')
+    args = parser.parse_args()
     
     # get config
     configs = config_all.create_config()
     '''
     To preprocess
     '''
-    # process vist data jsons and put it according to usage
-    process_vist.main(configs)
-    # extract vgg feats for all images. also remove images (and stories)
-    # for where images are not present
-    vggfeat_vist.main(configs)
-    # get coherence vector for all stories
-    get_cohvec_vist.main(configs)
-    
-    '''
-    Get stage 1 ready for nsi or cnsi
-    '''
-    # to pretrain model on mscoco dataset
-    cnsi.main(configs, 'pretrain')
-    # to train model stage 1 on vist dataset
-    cnsi.main(configs, 'trainstage1')
-    
-    '''
-    To Train CNSI model
-    '''
-    # to train cnsi model stage 2 on vist dataset
-    cnsi.main(configs, 'trainstage2', 'cnsi')
-    
-    '''
-    To Train NSI model
-    '''
-    # to train nsi model stage 2 on vist dataset
-    cnsi.main(configs, 'trainstage2', 'nsi')
-    
-    '''
-    To Train baseline model
-    '''
-    configs['model'] = 'baseline'
-    # to train baseline on vist dataset
-    baseline.main(configs, 'train')
-    
-    '''
-    To evaluate 'model' on VIST test set
-    '''
-    # get predictions for stories from testsamples for cnsi model
-    model2test =  configs['savemodel'] + 'stage2_cnsi_' + configs['date'] + '.h5'
-    cnsi.main(configs, 'test', 'cnsi', model2test)
-    
-    # get predictions for stories from testsamples for nsi model
-    model2test =  configs['savemodel'] + 'stage2_nsi_' + configs['date'] + '.h5'
-    cnsi.main(configs, 'test', 'nsi', model2test)
-    
-    # get predictions for stories from testsamples for baseline model
-    baseline.main(configs, 'test')
+    if args.preprocess:
+        # process vist data jsons and put it according to usage
+        process_vist.main(configs)
+        # extract vgg feats for all images. also remove images (and stories)
+        # for where images are not present
+        vggfeat_vist.main(configs)
+        # get coherence vector for all stories
+        get_cohvec_vist.main(configs)
     
     
+    if args.pretrain:
+        '''
+        Pretrain stage 1 on MSCOCO dataset
+        '''
+        cnsi.main(configs, 'pretrain')
+        
+    if args.train == 'stage1':
+        '''
+        train stage 1 on VIST dataset
+        '''
+        cnsi.main(configs, 'trainstage1')
+        
+    elif args.train == 'cnsi':
+        '''
+        To Train CNSI model stage 2
+        '''
+        cnsi.main(configs, 'trainstage2', 'cnsi')
+        
+    elif args.train == 'nsi':
+        '''
+        To Train NSI model stage 2
+        '''
+        cnsi.main(configs, 'trainstage2', 'nsi')
+        
+    elif args.train == 'baseline':
+        '''
+        To Train baseline model
+        '''
+        configs['model'] = 'baseline'
+        baseline.main(configs, 'train')
+    else:
+        raise ValueError('args for train can be stage1, cnsi, ' + 
+                         'nsi or baseline only')
+    
+    '''
+    To evaluate 'model' on VIST test set. Thiw will save predictions in file
+    for further use by metrics. Will not print or show any results.
+    '''
+    if args.eval == 'cnsi':
+        # get predictions for stories from testsamples for cnsi model
+        model2test =  (configs['savemodel'] + 'stage2_cnsi_' + 
+                       configs['date'] + '.h5')
+        cnsi.main(configs, 'test', 'cnsi', model2test)
+    elif args.eval == 'nsi':
+        # get predictions for stories from testsamples for nsi model
+        model2test =  (configs['savemodel'] + 'stage2_nsi_' + 
+                       configs['date'] + '.h5')
+        cnsi.main(configs, 'test', 'nsi', model2test)
+    elif args.eval == 'baseline':
+        # get predictions for stories from testsamples for baseline model
+        baseline.main(configs, 'test')
+    
+    
+    '''
+    To evaluate 'model' on VIST test set. Thiw will save predictions in file
+    for further use by metrics. Will not print or show any results.
+    '''
+    if args.show == 'cnsi':
+        # get predictions for stories from testsamples for cnsi model
+        model2test =  (configs['savemodel'] + 'stage2_cnsi_' + 
+                       configs['date'] + '.h5')
+        cnsi.main(configs, 'test', 'cnsi', model2test)
+    elif args.show == 'nsi':
+        # get predictions for stories from testsamples for nsi model
+        model2test =  (configs['savemodel'] + 'stage2_nsi_' + 
+                       configs['date'] + '.h5')
+        cnsi.main(configs, 'test', 'nsi', model2test)
+    elif args.show == 'baseline':
+        # get predictions for stories from testsamples for baseline model
+        baseline.main(configs, 'test')
     
     
     
